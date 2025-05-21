91021a9435ef8110212b5e6bd0f30cca413e7ecb
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-21 22:25:45 +0800

    implemented comment class and commentserializer class

--- web/catalog/serializers.py
+++ web/catalog/serializers.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from datetime import datetime
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+        
+#* Serializers Overview
+class Comment:
+    def __init__(self, email, content, created=None):
+        self.email = email
+        self.content = content
+        self.created = created or datetime.now()
+        
+class CommentSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=200)
+    created = serializers.DateTimeField()
