463f965ddb471361c3e41f62aaea257d04e23589
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-25 00:13:19 +0800

    fixed import bug in models

--- web/catalog/serializers.py
+++ web/catalog/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from datetime import datetime
+from catalog.models import Artist, Album, Song
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,6 +10,24 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email']
         
 class ArtistSerializer(serializers.ModelSerializer):
+    """
+    Automatically created PK field (ex: id: 1)
+    """
     class Meta:
         model = Artist
         fields = '__all__'
+        
+class ArtistHyperlinkSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Set a url field instead of a pk field (ex: url: 'http://api.example.com')
+    """
+
+class AlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Album
+        fields = '__all__'
+        
+class Song(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = '__all__ ' 
