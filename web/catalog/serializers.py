2a1014fec05940ab4e86d2f8d0765540ecdd96f3
razstvien01 <nicolenevanz.aricayos@gmail.com>
2025-05-24 23:56:21 +0800

    init Artist Serializer

--- web/catalog/serializers.py
+++ web/catalog/serializers.py
@@ -1,19 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from datetime import datetime
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
         
-#* Serializers Overview
-class Comment:
-    def __init__(self, email, content, created=None):
-        self.email = email
-        self.content = content
-        self.created = created or datetime.now()
-        
-class CommentSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    content = serializers.CharField(max_length=200)
-    created = serializers.DateTimeField()
+class ArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = '__all__'
